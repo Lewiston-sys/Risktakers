540a5ac9d068fba09d7d48474a5498c19ac7d0f0
Lewiston-sys <lmugo476@gmail.com>
2026-09-20 16:22:09 -0200

    Implement WebSocket connection to Deriv API

--- app.js
+++ app.js
@@ -1,3 +1,21 @@
-function connectDeriv() {
-  alert("Deriv connection will be connected here.");
-}
+const ws = new WebSocket(
+  "wss://ws.derivws.com/websockets/v3?app_id=1089"
+);
+
+ws.onopen = () => {
+  ws.send(JSON.stringify({
+    ticks: "R_100",
+    subscribe: 1
+  }));
+};
+
+ws.onmessage = (event) => {
+  const data = JSON.parse(event.data);
+  if (data.tick) {
+    console.log("R_100 price:", data.tick.quote);
+  }
+};
+
+ws.onerror = () => {
+  console.log("Deriv connection error");
+};
